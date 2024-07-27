9ff9d353b7dce8e74bed547fb70c6e38d00dae5f
Matheus Silva <msilvaqs@outlook.com>
2024-07-27 12:39:40 -0400

    Routing application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,49 @@
-import { Routes } from '@angular/router';
+import {
+  CanMatchFn,
+  RedirectCommand,
+  Route,
+  Router,
+  Routes,
+  UrlSegment,
+} from '@angular/router';
+import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import {
+  UserTasksComponent,
+  resolveTitle,
+  resolveUserName,
+} from './users/user-tasks/user-tasks.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route: Route, segments: UrlSegment[]) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess) {
+    return true;
+  }
+
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+};
 
 export const routes: Routes = [
   {
     path: '',
     component: NoTaskComponent,
+    title: 'No task selected',
   },
   {
     path: 'users/:userId',
     component: UserTasksComponent,
+    children: userRoutes,
+    canMatch: [dummyCanMatch],
+    resolve: {
+      userName: resolveUserName,
+    },
+    title: resolveTitle,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,66 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
+import { UsersService } from '../users.service';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterLink,
+  RouterOutlet,
+  RouterStateSnapshot,
+} from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  userName = input.required<string>();
+  // userId = input.required<string>();
+  // userNameParams = '';
+  // private usersService = inject(UsersService);
+  // private activatedRoute = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef);
+  // userName = computed(
+  //   () =>
+  //     this.usersService.users.find((user) => user.id === this.userId())?.name
+  // );
+  // ngOnInit(): void {
+  //   const subscription = this.activatedRoute.paramMap.subscribe({
+  //     next: (paramMap) =>
+  //       (this.userNameParams =
+  //         this.usersService.users.find(
+  //           (user) => user.id === paramMap.get('userId')
+  //         )?.name ?? ''),
+  //   });
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  // }
+}
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  _routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName =
+    usersService.users.find(
+      (user) => user.id === activatedRoute.paramMap.get('userId')
+    )?.name ?? '';
+  return userName;
+};
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  return resolveUserName(activatedRoute, routerState) + "'s Tasks";
+};
